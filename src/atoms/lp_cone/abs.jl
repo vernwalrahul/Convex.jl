c212b37aa7194a8cb3e9e6ffe551da243897a8b6
Rahul <vernwalrahul@gmail.com>
2017-03-20 12:44:03 +0530

    reverting

--- src/atoms/lp_cone/abs.jl
+++ src/atoms/lp_cone/abs.jl
@@ -6,7 +6,7 @@
 #############################################################################
 
 import Base.abs
-export abs, abs2
+export abs, square_modulus
 export sign, curvature, monotonicity, evaluate
 
 ### Absolute Value
@@ -60,4 +60,4 @@ function conic_form!(x::AbsAtom, unique_conic_forms::UniqueConicForms)
 end
 
 abs(x::AbstractExpr) = AbsAtom(x)
-abs2(x::AbstractExpr) = square(abs(x))
+square_modulus(x::AbstractExpr) = square(abs(x))
